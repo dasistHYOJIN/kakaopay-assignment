df64e371149f9a90010ffe25d938a3ccd5407bff
박효진 <hgenie14@gmail.com>
2019-12-12 19:40:39 +0900

    feat: Guarantee 엔티티와 Institute 엔티티 연관관계 매핑

--- src/test/java/com/ehem/kakaopay/guarantee/domain/GuaranteeTest.java
+++ src/test/java/com/ehem/kakaopay/guarantee/domain/GuaranteeTest.java
@@ -1,6 +1,8 @@
 package com.ehem.kakaopay.guarantee.domain;
 
 import com.ehem.kakaopay.guarantee.domain.vo.Amount;
+import com.ehem.kakaopay.institute.domain.Institute;
+import com.ehem.kakaopay.institute.domain.vo.InstituteType;
 import com.ehem.kakaopay.guarantee.domain.vo.Month;
 import com.ehem.kakaopay.guarantee.domain.vo.Year;
 import org.junit.jupiter.api.Test;
@@ -14,11 +16,13 @@ class GuaranteeTest {
         Guarantee guarantee = Guarantee.builder()
                 .year(Year.of(2019))
                 .month(Month.of(12))
+                .institute(new Institute(InstituteType.WOORI))
                 .amount(Amount.of(79))
                 .build();
 
         assertThat(guarantee.getYear()).isEqualTo(Year.of(2019));
         assertThat(guarantee.getMonth()).isEqualTo(Month.of(12));
+        assertThat(guarantee.getInstitute()).isEqualTo(new Institute(InstituteType.WOORI));
         assertThat(guarantee.getAmount()).isEqualTo(Amount.of(79));
     }
 }
