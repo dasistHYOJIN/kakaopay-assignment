3eb1acaabd25f5e09ab07d64983b823819c50f36
박효진 <hgenie14@gmail.com>
2019-12-15 03:50:47 +0900

    feat: 각 년도별 각 기관의 전체 지원금액 중에서 가장 큰 금액의 금융 정보를 가져오는 쿼리문 작성

--- src/main/java/com/ehem/kakaopay/model/guarantee/repository/GuaranteeRepository.java
+++ src/main/java/com/ehem/kakaopay/model/guarantee/repository/GuaranteeRepository.java
@@ -2,6 +2,16 @@ package com.ehem.kakaopay.model.guarantee.repository;
 
 import com.ehem.kakaopay.model.guarantee.domain.Guarantee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GuaranteeRepository extends JpaRepository<Guarantee, Long> {
+
+    @Query(value = "SELECT t.year year, g.max max, t.institute_name institute_name " +
+            "FROM (SELECT institute_name, year, sum(amount) sum FROM guarantee GROUP BY institute_name, year) t " +
+            "JOIN (SELECT year, max(sum) max " +
+            "FROM (SELECT institute_name, year, sum(amount) sum FROM guarantee GROUP BY institute_name, year) s GROUP BY year) g " +
+            "ON t.sum = g.max", nativeQuery = true)
+    List<Object[]> findAllInstitutesByYearAndMaxTotalAmount();
 }
