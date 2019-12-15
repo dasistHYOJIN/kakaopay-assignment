26379476e4fbb203d419f3913a7b35a577b783aa
박효진 <hgenie14@gmail.com>
2019-12-15 18:23:42 +0900

    refactor: GuaranteeRepository 메서드 반환 타입 수정

--- src/main/java/com/ehem/kakaopay/model/guarantee/repository/GuaranteeRepository.java
+++ src/main/java/com/ehem/kakaopay/model/guarantee/repository/GuaranteeRepository.java
@@ -2,6 +2,7 @@ package com.ehem.kakaopay.model.guarantee.repository;
 
 import com.ehem.kakaopay.model.guarantee.domain.Guarantee;
 import com.ehem.kakaopay.model.guarantee.domain.vo.Year;
+import com.ehem.kakaopay.model.guarantee.service.dto.TotalAmountPerYearResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,10 +14,10 @@ public interface GuaranteeRepository extends JpaRepository<Guarantee, Long> {
     /**
      * 년도별 각 금융기관의 지원금액 합계
      **/
-    @Query(value = "SELECT institute_name, year, sum(amount) sum " +
-            "FROM guarantee " +
-            "GROUP BY institute_name, year", nativeQuery = true)
-    List<Object[]> findTotalAmountGroupByInstituteNameAndYear();
+    @Query(value = "SELECT new com.ehem.kakaopay.model.guarantee.service.dto.TotalAmountPerYearResult(g.year, g.institute, sum(g.amount)) " +
+            "FROM Guarantee g " +
+            "GROUP BY g.institute, g.year")
+    List<TotalAmountPerYearResult> findTotalAmountGroupByInstituteNameAndYear();
 
     /**
      * 각 년도별 가장 많은 금액을 지원한 기관명
---
+++ src/main/java/com/ehem/kakaopay/model/guarantee/service/dto/TotalAmountPerYearResult.java
@@ -0,0 +1,20 @@
+package com.ehem.kakaopay.model.guarantee.service.dto;
+
+import com.ehem.kakaopay.model.guarantee.domain.vo.Year;
+import com.ehem.kakaopay.model.institute.domain.Institute;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TotalAmountPerYearResult {
+    private Year year;
+    private Institute institute;
+    private long sum;
+
+    public TotalAmountPerYearResult(final Year year, final Institute institute, final long sum) {
+        this.year = year;
+        this.institute = institute;
+        this.sum = sum;
+    }
+}
